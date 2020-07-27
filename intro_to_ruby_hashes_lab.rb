d741f8522f61b01ece36e8f7ee4364f7413f03ee
Chana Kupetz <chanakupetz@gmail.com>
2020-07-27 04:09:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,9 +7,7 @@ end
 def monopoly_with_second_tier
   {:railroads => {
     :pieces => 4,
-    :rent_in_dollars => {
-      :one_piece_owned => 25
-    },
+    :rent_in_dollars => {},
     :names => {}
   }
 }
