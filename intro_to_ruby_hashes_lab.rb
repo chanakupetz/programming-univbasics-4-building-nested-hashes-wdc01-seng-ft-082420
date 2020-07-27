a69195560a967b79354034c052288d4b4af2f6f5
Chana Kupetz <chanakupetz@gmail.com>
2020-07-27 04:17:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -67,6 +67,5 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-#        :mortgage_value => 100
 
 end
